524362be9f3883c993195b84e92b4d530d09f143
Lawrence <mwirigimburugu81@gmail.com>
2024-02-07 11:44:32 +0300

    created user model

--- app/models.py
+++ app/models.py
@@ -3,3 +3,11 @@ from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    role = db.Column(db.String(50), nullable=False)
+    student = db.relationship('Student', backref='user', uselist=False)
+    teacher = db.relationship('Teacher', backref='user', uselist=False)
+    parent = db.relationship('Parent', backref='user', uselist=False)
